bf077551d7c3b76af9db9bff965165829cb89075
Chiyen Venegas <chi@marandino.dev>
2022-12-06 16:53:56 -0600

    linting

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function Layout({ children, pageTitle }: { children: React.ReactNode, pageTitle: string }) {
+export default function Layout({ children, pageTitle }: { children: React.ReactNode; pageTitle: string }) {
   return (
     <div>
       <Head>
