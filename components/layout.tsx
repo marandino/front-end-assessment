13af99633f64db9b4521ed4779160d90207917df
Chiyen Venegas <chi@marandino.dev>
2022-12-06 16:52:10 -0600

    add(layout): navbar w search box

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,17 +2,17 @@ import Head from "next/head";
 
 export default function Layout({ children, pageTitle }: { children: React.ReactNode, pageTitle: string }) {
   return (
-    <div className="h-screen">
+    <div>
       <Head>
         <title>{pageTitle}</title>
-        <meta
-          name={pageTitle}
-          content="Front end demo querying data from swapi.js"
-        />
+        <meta name={pageTitle} content="Front end demo querying data from swapi.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* TODO: add a Navbar for the Search Box */}
-      <main className="h-full" >{children}</main>
+      <nav className="p-4 w-full justify-center flex">
+        <input className="border-2 px-4 py-2" type="text" name="searchBar" id="searchBar" />
+        <button className="ml-4 border-2 px-4 py-2">Search</button>
+      </nav>
+      <main className="pt-16">{children}</main>
       <footer className="absolute bottom-px py-6 border-t-2 w-full text-center">
         <a href="https://github.com/marandino">This is a neat footer with useful information.</a>
       </footer>
