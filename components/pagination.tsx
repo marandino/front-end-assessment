47ce1c5ad65ddcb0fa56f0b4d3ac6d2a90b800ef
Chiyen Venegas <chi@marandino.dev>
2022-12-07 07:33:21 -0600

    fix(pagination): it going off the available pages

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,25 +1,30 @@
 export default function Pagination({ totalResults, currentIndex }: { totalResults: number; currentIndex: number }) {
+  const maxPages = Math.round(totalResults / 10);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-4 sm:px-6 w-full absolute bottom-20">
       <div className="flex flex-1 justify-between">
         <div>
           <p className="text-sm text-gray-700">
-            Showing page {currentIndex} out of {Math.round(totalResults / 10)}.
+            Showing page {currentIndex} out of {maxPages}.
           </p>
         </div>
         <div>
-          <a
-            href={`/?page=${currentIndex - 1}`}
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Previous
-          </a>
-          <a
-            href={`/?page=${currentIndex + 1}`}
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          >
-            Next
-          </a>
+          {currentIndex > 1 && (
+            <a
+              href={`/?page=${currentIndex - 1}`}
+              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            >
+              Previous
+            </a>
+          )}
+          {currentIndex < maxPages && (
+            <a
+              href={`/?page=${currentIndex + 1}`}
+              className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            >
+              Next
+            </a>
+          )}
         </div>
       </div>
     </div>
