300cd940c9bab24a3a9fa4e7714643bb0d3ea02f
Chiyen Venegas <chi@marandino.dev>
2022-12-07 07:23:53 -0600

    add(top bar): back button that goes to index

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,12 +9,15 @@ export default function Layout({ children, pageTitle }: { children: React.ReactN
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <nav className="p-4 w-full justify-center flex">
+        <a href="/">
+          <button className="mr-4 border-2 px-4 py-2">Back</button>
+        </a>
         <input className="border-2 px-4 py-2" type="text" name="searchBar" id="searchBar" />
         <button className="ml-4 border-2 px-4 py-2">Search</button>
       </nav>
       <main className="pt-16">{children}</main>
       <footer className="absolute bottom-px py-6 border-t-2 w-full text-center">
-        <a href="https://github.com/marandino">This is a neat footer with useful information.</a>
+        <a href="https://github.com/marandino">This is a neat footer with unuseful information.</a>
       </footer>
     </div>
   );
