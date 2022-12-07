591cfa32e815e97da0fe2333dd31fbe93ee63380
Chiyen Venegas <chi@marandino.dev>
2022-12-07 07:55:05 -0600

    fix(pagination): text and buttons being too small

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,20 +1,17 @@
 import Link from "next/link";
-
 export default function Pagination({ totalResults, currentIndex }: { totalResults: number; currentIndex: number }) {
   const maxPages = Math.round(totalResults / 10);
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-4 sm:px-6 w-full absolute bottom-20">
       <div className="flex flex-1 justify-between">
-        <div>
-          <p className="text-sm text-gray-700">
-            Showing page {currentIndex} out of {maxPages}.
-          </p>
-        </div>
+        <p className="text-gray-700 self-center ">
+          Showing page {currentIndex} out of {maxPages}.
+        </p>
         <div>
           {currentIndex > 1 && (
             <Link
               href={`/?page=${currentIndex - 1}`}
-              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+              className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 hover:bg-gray-50"
             >
               Previous
             </Link>
@@ -22,7 +19,7 @@ export default function Pagination({ totalResults, currentIndex }: { totalResult
           {currentIndex < maxPages && (
             <Link
               href={`/?page=${currentIndex + 1}`}
-              className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+              className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 hover:bg-gray-50"
             >
               Next
             </Link>
